4ca4944819f96455568bf9645e9f53f5119e421b
YuanMing.Zhang <zyuanming@outlook.com>
2015-02-06 13:15:05 +0800

    Initial Commit

---
+++ SpringApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpringApp
+//
+//  Created by ming on 15/2/6.
+//  Copyright (c) 2015年 yooke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
